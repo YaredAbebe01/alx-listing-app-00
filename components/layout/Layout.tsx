07f3b40af5e46e230dc569945fac36c472b3f278
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2025-11-29 21:12:23 +0300

    Create Layout.tsx

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+// components/layout/Layout.tsx
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen bg-white">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
